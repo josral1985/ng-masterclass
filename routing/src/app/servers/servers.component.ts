4516360d4360a28d8936b8e30ca8059724c42b5c
Jose Hernandez <jose@markojudas.com>
2023-07-02 09:14:27 -0400

    dealing with relative and absolute paths

--- routing/src/app/servers/servers.component.ts
+++ routing/src/app/servers/servers.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  public servers: {id: number, name: string, status: string}[] = [];
+  public servers: { id: number; name: string; status: string }[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
 
+  onReload() {
+    // this.router.navigate(['/servers']); absolute path
+    // this.router.navigate(['servers']); relative path. .navigate doesn't know where route I am currently.
+    // this.router.navigate(['servers'], { relativeTo: this.route }); relative path telling that is relative to this route. (/servers/servers)
+  }
 }
