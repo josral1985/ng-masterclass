c8d39a73185d48bb7aaf4eb8b7518642942a1202
Jose R Hernandez <jose_r_hernandez@progressive.com>
2023-09-18 13:54:57 -0400

    form created and controls resistered for Template Driven approach

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule, // needed to get the Template Driven Approach
   ],
   providers: [],
   bootstrap: [AppComponent]
