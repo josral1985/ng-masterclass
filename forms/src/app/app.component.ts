bf0f459896aea20128fe0733266c50ee43bdc545
Jose R Hernandez <jose_r_hernandez@progressive.com>
2023-09-19 09:41:43 -0400

    TD: set form values; not best apporach

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -13,6 +13,15 @@ export class AppComponent {
   genders = ['male', 'female'];
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupForm.setValue({
+      userData: {
+        username: suggestedName,
+        email: ""
+      },
+      secret: "pet",
+      questionAnswer: "",
+      gender: "male"
+    });
   }
 
   // onSubmit(form: NgForm) {
