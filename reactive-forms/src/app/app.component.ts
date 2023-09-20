9beffe3836143058d848ec272a9f07a3d6324796
Jose R Hernandez <jose_r_hernandez@progressive.com>
2023-09-20 09:40:35 -0400

    Reactive Forms: Creating Form in code

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
 
   // Property to hold Form
   signUpForm: FormGroup;
+
+  ngOnInit(): void {
+      //this.signUpForm = new FormGroup({});  // create empty form without any controls
+      this.signUpForm = new FormGroup({
+        // add controls
+        'username': new FormControl(null),  //formState null -> default value
+        'email': new FormControl(null),
+        'gender': new FormControl('male')
+      })
+  }
 }
