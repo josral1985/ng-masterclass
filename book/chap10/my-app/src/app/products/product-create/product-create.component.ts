ca8027c30c179c895093e43a3b68347bfa50d421
Jose R Hernandez <jose_r_hernandez@progressive.com>
2023-09-28 14:24:37 -0400

    Book: Interacting with reactive forms

--- book/chap10/my-app/src/app/products/product-create/product-create.component.ts
+++ book/chap10/my-app/src/app/products/product-create/product-create.component.ts
@@ -1,22 +1,38 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Product } from '../product';
 import { ProductsService } from '../products.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
-  styleUrls: ['./product-create.component.css']
+  styleUrls: ['./product-create.component.css'],
 })
 export class ProductCreateComponent {
+  productForm = new FormGroup({
+    name: new FormControl('', { nonNullable: true }),
+    price: new FormControl<number | undefined>(undefined, {
+      nonNullable: true,
+    }),
+  });
 
   @Output() added = new EventEmitter<Product>();
 
   constructor(private productsService: ProductsService) {}
 
-  createProduct(name: string, price: number) {
-    this.productsService.addProduct(name, price).subscribe(product => {
-      this.added.emit(product);
-    });
+  get name() {
+    return this.productForm.controls.name;
+  }
+  get price() {
+    return this.productForm.controls.price;
   }
 
+  createProduct() {
+    this.productsService
+      .addProduct(this.name.value, Number(this.price.value))
+      .subscribe((product) => {
+        this.productForm.reset();
+        this.added.emit(product);
+      });
+  }
 }
