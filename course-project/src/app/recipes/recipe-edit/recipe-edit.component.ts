f10e6b4a44ecbc7ef3eb53b6308a4c4131ce5f6a
Jose R Hernandez <jose_r_hernandez@progressive.com>
2023-10-05 15:16:09 -0400

    Project -> Forms: Creating the Form for Editing Recipes (reactive)

--- course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,13 +11,39 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false; //are we adding a new? or editing?
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) {}
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
-      this.id = +params['id'];
+      this.id = +params['id']; //casting string to number
       this.editMode = params['id'] != null; //if we pass an actual id this will be true (edit mode); other wise false (adding new recipe)
+      this.initForm();
+    });
+  }
+
+
+  //to be called whenever our route params change
+  private initForm() {
+    // our we in edit mode?
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+
+    if (this.editMode) {
+      // inject the service on construction and get the recipe
+      // get the recipe using service
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+    } 
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImagePath),
+      'description': new FormControl(recipeDescription)
     });
   }
 }
